4429a58c5c790fb3cf8ba2d8d965d999c5bd71a5
free0ne <korianets@gmail.com>
2018-12-16 19:18:02 +0300

    new colors

--- menu.lua
+++ menu.lua
@@ -19,20 +19,28 @@ local scene = composer.newScene()
 -- create()
 function scene:create( event )
 
+	local font = "amsterdam.otf"
+
+	primaryColor = {0/255, 96/255, 100/255}
+	secondaryColor = {0/255, 53/255, 57/255}
+	fillColor = {1, 1, 1}
+
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
-  display.setDefault( "background", 240/255, 240/255,245/255 )
+  display.setDefault( "background", 255/255, 255/255, 255/255 )
 
 --##################### upper tab goes here ####################
 
-  settings = display.newImage( sceneGroup, "pics/settings.png", 40, 40)
-
-  local coins = display.newImage( sceneGroup, "pics/school.png", 500, 40)
-  local cross = display.newImage( sceneGroup, "pics/multiplyb.png", 445, 40)
-  cross:scale(0.9,0.9)
+  settings = display.newImage( sceneGroup, "pics/settings_whote.png", 40, 40)
+  settings:setFillColor( unpack(secondaryColor) )
+  local coins = display.newImage( sceneGroup, "pics/school_white.png", 500, 40)
+	coins:setFillColor( unpack(secondaryColor) )
+  local cross = display.newImage( sceneGroup, "pics/multiply_white.png", 445, 40)
+  cross:scale(0.6,0.6)
+	cross:setFillColor( unpack(secondaryColor) )
   coinCount = 0
-  local coinText = display.newText( sceneGroup, coinCount, 410, 40, native.systemFont, 46 )
-  coinText:setFillColor( 0, 0, 0 )
+  local coinText = display.newText( sceneGroup, coinCount, 410, 40, font, 46 )
+  coinText:setFillColor( unpack(secondaryColor) )
 
 --------------------- upper tab ------------------------
 --#################### test buttons #####################
@@ -40,27 +48,27 @@ function scene:create( event )
   testGroup1.x = display.contentCenterX
   testGroup1.y = 200
   local testRect1 = display.newRect( testGroup1, 0, 0, 450, 120 )
-  testRect1:setFillColor(66/255, 134/255, 244/255)
-  local testLabel1 = display.newText( testGroup1, "Название 1", -130, -35, native.systemFont, 30 )
-  testLabel1:setFillColor( 0, 0, 0 )
+  testRect1:setFillColor(unpack(primaryColor))
+  local testLabel1 = display.newText( testGroup1, "Тест Айзенка", -115, -35, font, 30 )
+  testLabel1:setFillColor( unpack(fillColor) )
 
-  local tasks1 = display.newImage( testGroup1, "pics/question.png", -190, 5)
+  local tasks1 = display.newImage( testGroup1, "pics/question_white.png", -190, 5)
   tasks1:scale(0.7,0.7)
-  local testTaskCount1 = display.newText( testGroup1, ":20", -150, 5, native.systemFont, 30 )
-  testTaskCount1:setFillColor( 0, 0, 0 )
+  local testTaskCount1 = display.newText( testGroup1, ":20", -150, 5, font, 30 )
+  testTaskCount1:setFillColor( unpack(fillColor) )
 
-  local time1 = display.newImage( testGroup1, "pics/time.png", -80, 5)
+  local time1 = display.newImage( testGroup1, "pics/time_white.png", -80, 5)
   time1:scale(0.7,0.7)
-  local timeAmount1 = display.newText( testGroup1, ":20 мин", -10, 5, native.systemFont, 30 )
-  timeAmount1:setFillColor( 0, 0, 0 )
+  local timeAmount1 = display.newText( testGroup1, ":20 мин", -10, 5, font, 30 )
+  timeAmount1:setFillColor( unpack(fillColor) )
 
-  local help1 = display.newImage( testGroup1, "pics/help.png", 180, 0)
+  local help1 = display.newImage( testGroup1, "pics/help_white.png", 180, 0)
   time1:scale(0.7,0.7)
 
-  local scores1 = display.newImage( testGroup1, "pics/star.png", -45, 40)
+  local scores1 = display.newImage( testGroup1, "pics/star_white.png", -45, 40)
   scores1:scale(0.5,0.5)
-  local scoresText1 = display.newText( testGroup1, "98", -5, 40, native.systemFont, 28 )
-  scoresText1:setFillColor( 0, 0, 0 )
+  local scoresText1 = display.newText( testGroup1, "98", -5, 40, font, 28 )
+  scoresText1:setFillColor( unpack(fillColor) )
 
 
 --------------------- test buttons ------------------------
